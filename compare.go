954f8afed90fbbf3883483a8d30580884ae0d9e1
boozoorg <rajabovbuzurg22@gmail.com>
2024-06-02 21:52:29 +0500

    compare pointers

--- compare.go
+++ compare.go
@@ -1,8 +1,21 @@
 package compare
 
+import (
+	"fmt"
+	"reflect"
+)
+
 // compare to comparable type
 func IsEqual[K comparable](first, second K) bool {
+	if reflect.ValueOf(first).Kind() == reflect.Pointer {
+		return fmt.Sprint(getDataFromPointer(reflect.ValueOf(first))) == fmt.Sprint(getDataFromPointer(reflect.ValueOf(second)))
+	}
 	return first == second
 }
 
-// Todo:add compare of to not comparable type
+func getDataFromPointer(v reflect.Value) reflect.Value {
+	if v.Kind() == reflect.Pointer {
+		return getDataFromPointer(v.Elem())
+	}
+	return v
+}
